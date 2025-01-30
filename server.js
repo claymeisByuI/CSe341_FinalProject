0236c8bdc25546af38cfe14534adff849155bcc1
Clayton M <clayton@txdocs.com>
2025-01-29 22:40:05 -0600

    Clean up

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require('express'); // Import express
 const app = express(); // Initialize express
 
 const port = process.env.PORT || 3000; // Port number
-app.use(bodyParser.json());
-app.use('/', routes);
+//app.use('/', routes);
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
